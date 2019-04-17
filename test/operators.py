34ff9cda54f5b33b4dd0ccc09bfa1e36658db4cc
Bram Wasti <bwasti@fb.com>
2019-04-17 13:16:37 -0700

    [operator] Add batch norm

--- test/operators.py
+++ test/operators.py
@@ -2,6 +2,7 @@ import unittest
 from util import TestCase
 
 import torch.nn.functional as F
+import torch
 
 # test jit tvm operators
 class TestOperators(TestCase):
@@ -23,6 +24,25 @@ class TestOperators(TestCase):
             return F.conv2d(a, b) + c
 
         self.checkTraceTVM(conv, input_shapes=[[20,16,10,10], [10,16,3,3], [20,10,8,8]], verbose=True)
+
+    def test_batch_norm(self):
+        def batch_norm(a, b, c, d):
+            return F.batch_norm(a + d, b, c)
+
+        a = torch.rand(10,10)
+        b = torch.rand(10)
+        c = torch.rand(10)
+        d = torch.rand(10,10)
+
+        self.checkTraceTVM(batch_norm,
+          input_tensors=[a,b,c,d], verbose=True)
+
+        def batch_norm_weighted(a, b, c, d, weight, bias):
+            return F.batch_norm(a + d, b, c, weight=weight, bias=bias)
+
+        self.checkTraceTVM(batch_norm_weighted,
+          input_tensors=[a,b,c,d,c,b], verbose=True)
+
 
 if __name__ == '__main__':
     unittest.main()
