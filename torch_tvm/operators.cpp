daabe2117074fbf277260bb12e1c0aaaebd7ee42
Wanchao Liang <wanchaol@users.noreply.github.com>
2019-07-09 23:14:29 -0700

    [codemod] deprecated macros

--- torch_tvm/operators.cpp
+++ torch_tvm/operators.cpp
@@ -74,10 +74,10 @@ RegisterTVMOperator::RegisterTVMOperator(std::vector<TVMOpMap> ops) {
 
 // This must be done lazily to prevent SIOF
 void registerSchedule(std::string name) {
-  AT_ASSERT(getTVMScheduleMap().find(name) != getTVMScheduleMap().end());
+  TORCH_INTERNAL_ASSERT(getTVMScheduleMap().find(name) != getTVMScheduleMap().end());
   TVMScheduleFunctor sched_f = getTVMScheduleMap()[name];
   auto reg = tvm::runtime::Registry::Get("relay.op._Register");
-  AT_ASSERT(reg);
+  TORCH_INTERNAL_ASSERT(reg);
   auto sched = sched_f();
   // Relay does not provide a good API for querying the status of schedules
   if (sched) {
@@ -94,8 +94,8 @@ bool isConstant(tvm::relay::Expr e) {
 template <typename T>
 T relayToConstant(tvm::relay::Expr e) {
   auto c = e.as<tvm::relay::ConstantNode>();
-  AT_ASSERT(c);
-  AT_ASSERT(c->is_scalar());
+  TORCH_INTERNAL_ASSERT(c);
+  TORCH_INTERNAL_ASSERT(c->is_scalar());
   return static_cast<T*>(c->data->data)[0];
 }
 
@@ -139,11 +139,11 @@ RegisterTVMOperator reg({
     {Symbol::fromQualString("aten::add"),
      [](Node* node, tvm::Array<tvm::relay::Expr> inputs) {
        auto op = tvm::relay::Op::Get("add");
-       AT_ASSERT(inputs.size() == 3);
+       TORCH_INTERNAL_ASSERT(inputs.size() == 3);
        tvm::Array<tvm::relay::Expr> add_inputs = {inputs[0], inputs[1]};
        // Handle pytorch's value argument in add
        auto value = inputs[2].as<tvm::relay::ConstantNode>();
-       AT_ASSERT(
+       TORCH_INTERNAL_ASSERT(
            value->is_scalar() &&
            reinterpret_cast<int*>(value->data->data)[0] == 1);
        auto out = tvm::relay::CallNode::make(op, add_inputs, tvm::Attrs(), {});
@@ -152,11 +152,11 @@ RegisterTVMOperator reg({
     {Symbol::fromQualString("aten::add_"),
      [](Node* node, tvm::Array<tvm::relay::Expr> inputs) {
        auto op = tvm::relay::Op::Get("add");
-       AT_ASSERT(inputs.size() == 3);
+       TORCH_INTERNAL_ASSERT(inputs.size() == 3);
        tvm::Array<tvm::relay::Expr> add_inputs = {inputs[0], inputs[1]};
        // Handle pytorch's value argument in add
        auto value = inputs[2].as<tvm::relay::ConstantNode>();
-       AT_ASSERT(
+       TORCH_INTERNAL_ASSERT(
            value->is_scalar() &&
            reinterpret_cast<int*>(value->data->data)[0] == 1);
        auto out = tvm::relay::CallNode::make(op, add_inputs, tvm::Attrs(), {});
@@ -189,7 +189,7 @@ RegisterTVMOperator reg({
        if (const tvm::relay::VarNode* var =
                inputs[1].as<tvm::relay::VarNode>()) {
          auto* w_t = var->type_annotation.as<tvm::relay::TensorTypeNode>();
-         AT_ASSERT(w_t);
+         TORCH_INTERNAL_ASSERT(w_t);
          auto shape = w_t->shape;
          tvm::Array<tvm::relay::IndexExpr> w_sizes = {shape[2], shape[3]};
          conv_attrs->kernel_size = w_sizes;
@@ -218,12 +218,12 @@ RegisterTVMOperator reg({
     {Symbol::fromQualString("aten::batch_norm"),
      [](Node* node, tvm::Array<tvm::relay::Expr> inputs) -> tvm::relay::Expr {
        auto op = tvm::relay::Op::Get("nn.batch_norm");
-       AT_CHECK(
+       TORCH_CHECK(
            inputs.size() == 9,
            "batch_norm received ",
            inputs.size(),
            " inputs");
-       AT_CHECK(
+       TORCH_CHECK(
            relayToConstant<bool>(inputs[5]) == false,
            "batch_norm is in training mode");
        auto attrs = tvm::make_node<tvm::relay::BatchNormAttrs>();
@@ -268,7 +268,7 @@ RegisterTVMOperator reg({
 
        auto out =
            tvm::relay::CallNode::make(op, bn_inputs, tvm::Attrs(attrs), {});
-       AT_ASSERT(node->outputs().size() == 1);
+       TORCH_INTERNAL_ASSERT(node->outputs().size() == 1);
        if (node->outputs().size() == 2) {
          return out;
        }
@@ -292,20 +292,20 @@ RegisterTVMOperator reg({
      "relu"},
     {Symbol::fromQualString("aten::threshold_"),
      [](Node* node, tvm::Array<tvm::relay::Expr> inputs) {
-       AT_CHECK(!relayIsNone(inputs[0]));
-       AT_CHECK(!relayIsNone(inputs[1]));
-       AT_CHECK(!relayIsNone(inputs[2]));
+       TORCH_CHECK(!relayIsNone(inputs[0]));
+       TORCH_CHECK(!relayIsNone(inputs[1]));
+       TORCH_CHECK(!relayIsNone(inputs[2]));
        auto d = relayToConstant<float>(inputs[1]);
-       AT_CHECK(
+       TORCH_CHECK(
            d < 1e-7, "aten::threshold_ only supported for threshold 0, got", d);
-       AT_CHECK(
+       TORCH_CHECK(
            d > -1e-7,
            "aten::threshold_ only supported for threshold 0, got",
            d);
        d = relayToConstant<float>(inputs[2]);
-       AT_CHECK(
+       TORCH_CHECK(
            d < 1e-7, "aten::threshold_ only supported for value 0, got", d);
-       AT_CHECK(
+       TORCH_CHECK(
            d > -1e-7, "aten::threshold_ only supported for value 0, got", d);
        auto op = tvm::relay::Op::Get("nn.relu");
        auto out = tvm::relay::CallNode::make(op, {inputs[0]}, tvm::Attrs(), {});
@@ -375,7 +375,7 @@ RegisterTVMOperator reg({
        auto op = tvm::relay::Op::Get("reshape");
        auto attrs = tvm::make_node<tvm::relay::ReshapeAttrs>();
        attrs->newshape = relayToArray<tvm::Integer>(inputs[1]);
-       AT_ASSERT(attrs->newshape.size() > 0);
+       TORCH_INTERNAL_ASSERT(attrs->newshape.size() > 0);
        if (static_cast<int64_t>(attrs->newshape[0]) != -1) {
 	       std::cerr << "WARNING: reshape with -1 as the first value has known incompatibility with PyTorch semantics.\n";
        }
@@ -414,6 +414,6 @@ bool isSupported(Node* node) {
 }
 
 tvm::relay::Expr getOperator(Node* node, tvm::Array<tvm::relay::Expr> inputs) {
-  AT_ASSERT(isSupported(node));
+  TORCH_INTERNAL_ASSERT(isSupported(node));
   return getTVMOperatorMap()[node->kind()](node, inputs);
 }
