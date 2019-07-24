f4e7c30de9a3762e3c7873dd14b85f8d23e2b24b
kimishpatel <kimishpatel@devvm064.atn5.facebook.com>
2019-07-24 11:17:33 -0700

    Customized layer norm for pytorch tvm repo. Summary: This PR adds layer norm topi op. Provides correponding relay op. Adds test cases.
    Test Plan:
    python custom_tvm_ops/tests/layer_norm_test.py
    
    Reviewers:
    
    Subscribers:
    
    Tasks:
    
    Tags:

--- custom_tvm_ops/attrs/layer_norm_attrs.h
+++ custom_tvm_ops/attrs/layer_norm_attrs.h
@@ -7,9 +7,11 @@ namespace tvm {
 namespace relay {
 struct CustomLayerNormAttrs : public tvm::AttrsNode<CustomLayerNormAttrs> {
   Array<Integer> axis;
-  bool affine{false};
-  TVM_DECLARE_ATTRS(CustomLayerNormAttrs, "relay.attrs.CustomLayerNormAttrs") {
+  bool affine;
+  TVM_DECLARE_ATTRS(CustomLayerNormAttrs,
+      "relay.attrs.CustomLayerNormAttrs") {
     TVM_ATTR_FIELD(axis).set_default(NullValue<Array<Integer>>());
+    TVM_ATTR_FIELD(affine).set_default(false);
   }
 };
 }
