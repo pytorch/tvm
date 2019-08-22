2f8b4cf80cecb5a1a1cfa125a7293bf5d2d7f4ee
lingyiliu <lingyiliu@fb.com>
2019-08-22 00:33:57 -0700

    clean up the code

--- torch_tvm/fuse_concat.cpp
+++ torch_tvm/fuse_concat.cpp
@@ -3,22 +3,29 @@
 
 using namespace torch::jit;
 
+const size_t subgraph_arg_limit_ = 128;
+
 bool isFusableCatNode(Node* node) {
-  if (node->kind() != aten::cat)
+  if (node->kind() != aten::cat) {
     return false;
-  if (!node->is_constant(attr::dim))
+  }
+  if (!node->is_constant(attr::dim)) {
     return false;
+  }
 
   auto tensors_node = node->namedInput(attr::tensors)->node();
   if ((tensors_node->inputs().size() + node->outputs().size()) >
-      128) {
+      subgraph_arg_limit_) {
     return false;
   }
-  if (tensors_node->kind() != prim::ListConstruct)
+  if (tensors_node->kind() != prim::ListConstruct) {
     return false;
+  }
 
-  if (tensors_node->output()->uses().size() > 1)
+  if (tensors_node->output()->uses().size() > 1) {
     return false;
+  }
+
   return true;
 }
 
@@ -32,28 +39,27 @@ Node* createFusedConcat(Node* node) {
                         ->i_(attr::dim, dim);
   fused_cat->insertBefore(list_construct);
   fused_cat->output()->copyMetadata(node->output());
+
   node->output()->replaceAllUsesWith(fused_cat->output());
+  node->destroyCurrent();
   if (list_construct->output()->uses().empty()) {
     list_construct->destroy();
   }
   return fused_cat;
 }
 
-void fuseConcats(Block* block_, AliasDb& aliasDb) {
+void fuseConcats(Block* block_) {
   for (auto it = block_->nodes().rbegin(); it != block_->nodes().rend();
        ++it) {
     Node* cat = *it;
     if (!isFusableCatNode(cat)) {
       continue;
     }
-
-    Node* list_construct = cat->namedInput(attr::tensors)->node();
     createFusedConcat(cat);
   }
 }
 
 void FuseConcat(std::shared_ptr<Graph> graph) {
-  AliasDb aliasDb(graph);
   auto block = graph->block();
-  fuseConcats(block, aliasDb);
+  fuseConcats(block);
 }
