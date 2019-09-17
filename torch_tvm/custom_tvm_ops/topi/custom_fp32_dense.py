e66fc8a04ef2b659470e088ae0066e5d88477d1c
Kimish Patel <kimishpatel@fb.com>
2019-09-17 09:56:56 -0700

    Enabled const partitioning so that loops can be partitioned by a given factor and the remainder. Aids performance.
    Summary:
    
    Test Plan:
    
    Reviewers:
    
    Subscribers:
    
    Tasks:
    
    Tags:

--- torch_tvm/custom_tvm_ops/topi/custom_fp32_dense.py
+++ torch_tvm/custom_tvm_ops/topi/custom_fp32_dense.py
@@ -14,11 +14,6 @@ def _declaration_custom_dense(cfg, data, packed_weight, bias, out_dtype=None):
     batch, in_dim = get_const_tuple(data.shape)
     pack_outer, _, pack_width = get_const_tuple(packed_weight.shape)
     out_dim = pack_outer * pack_width
-    # create tuning space
-    cfg.define_split("tile_y", batch, num_outputs=3)
-    cfg.define_split("tile_x", out_dim, num_outputs=3)
-    cfg.define_split("tile_k", in_dim, num_outputs=2)
-    _custom_dense_pack_config(cfg, batch, out_dim, in_dim)
 
     k = tvm.reduce_axis((0, in_dim), name="k")
     C = tvm.compute((batch, out_dim),
@@ -43,81 +38,33 @@ def _schedule_custom_dense_pack(cfg, outs):
     traverse_inline(s, outs[0].op, _callback)
     return s
 
-def _get_sorted_factors(x):
-    factors = []
-    n = 1
-    while n <= x:
-        if x % n == 0:
-            factors.append(n)
-        n += 1
-    factors.sort(reverse=True)
-    return factors
-
-def _get_largest_factor_smaller_than(num, max_value, preferred, min_value):
-    sorted_factors = _get_sorted_factors(num)
-    print(sorted_factors)
-    max_f = None
-    preferred_f = None
-    min_f = None
-    for f in sorted_factors:
-        if max_f is None and f <= max_value:
-            max_f = f
-        if preferred_f is None and f <= preferred and f > min_value:
-            preferred_f = f
-        if min_f is None and f <= min_value:
-            min_f = f
-    if preferred_f is not None:
-        return preferred_f
-    if max_f is not None:
-        return max_f
-    if min_f is not None:
-        return min_f
-    return 1
-
-def _custom_dense_pack_config(cfg, M, N, K):
-    vec_width = get_fp32_len()
-
-    tilex_ii = 1
-    for bn in range(vec_width*2, 0, -1):
-        if N % bn == 0:
-            tilex_ii = bn
-            break
-    NN = N // tilex_ii
-    tilex_oi = 1
-    while NN // tilex_oi > 4:
-        if (NN // tilex_oi) % 2 == 1:
-            break
-        tilex_oi *= 2
-
-    tiley_ii = _get_largest_factor_smaller_than(M, 12, 8, 4)
-    tiley_outer = M // tiley_ii
-    print("Y sizes:", tiley_outer, tiley_ii)
-    cfg["tile_y"] = SplitEntity([tiley_outer, 1, tiley_ii])
-    cfg["tile_x"] = SplitEntity([NN // tilex_oi, tilex_oi, tilex_ii])
-    cfg["tile_k"] = SplitEntity([K, 1])
 
 def _schedule_custom_dense_pack_template(cfg, s, C):
     CC = s.cache_write(C, "global")
     y, x = s[C].op.axis
     k, = s[CC].op.reduce_axis
 
-    yt, yo, yi = cfg["tile_y"].apply(s, C, y)
-    xt, xo, xi = cfg["tile_x"].apply(s, C, x)
+    data = C.op.input_tensors[0]
+    weight = C.op.input_tensors[1]
+    _, _, pack_factor = get_const_tuple(weight.shape)
+
+    xo, xi = s[C].split(x, factor=pack_factor)
+    yo, yi = s[C].split(y, factor=8)
+    xt, xo = s[C].split(xo, factor=16)
+    yt, yo = s[C].split(yo, factor=16)
     s[C].reorder(yt, xt, yo, xo, yi, xi)
-    s[C].fuse(yt, xt)
-    xyo = s[C].fuse(yo, xo)
     s[C].unroll(yi)
     s[C].vectorize(xi)
 
-    s[CC].compute_at(s[C], xyo)
+    s[CC].compute_at(s[C], xo)
     y, x = s[CC].op.axis
-    ko, ki = cfg["tile_k"].apply(s, CC, k)
+    ko, ki = s[CC].split(k, factor=16)
     s[CC].reorder(ko, ki, y, x)
     s[CC].vectorize(x)
     s[CC].unroll(y)
-    s[CC].unroll(ki)
 
     return s
+
 
 
 def dense_weight_pack(weight, pack_width):
